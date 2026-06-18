a3e285ed6a6a527f98617c5c490ba8c46308da14
miarva-enjoyer <murlintik@gmail.com>
2026-06-18 19:00:23 +0300

    Added the second part of the second lab

---
+++ Async and sync server shenanigans/Async and sync server shenanigans/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async_and_sync_server_shenanigans
+{
+    internal class Fakeserver
+    {
+
+        public Fakeserver()
+        {
+        }
+
+        public int Fakerequest()
+        {
+
+            Random rand = new Random();
+            int randomInt = rand.Next(1, 101);
+
+            Thread.Sleep(3000);
+
+            return randomInt;
+        }
+        public async Task<int> FakerequestAsync()
+        {
+
+            Random rand = new Random();
+            int randomInt = rand.Next(1, 101);
+
+            await Task.Delay(3000);
+
+            return randomInt;
+        }
+
+    }
+}
---
+++ Async and sync server shenanigans/Async and sync server shenanigans/sync.cs
@@ -0,0 +1,47 @@
+﻿using Async_and_sync_server_shenanigans;
+using System;
+using System.Diagnostics;
+
+Console.WriteLine("=== СИНХРОННАЯ ВЕРСИЯ ===");
+var stopwatch = Stopwatch.StartNew();
+
+
+    Fakeserver server = new Fakeserver();
+
+    // Первый запрос  
+    Console.WriteLine("Отправка запроса 1...");
+    int result1 = server.Fakerequest();
+    Console.WriteLine($"Запрос 1 завершен. Ответ: {result1}.");
+
+// Второй запрос  
+Console.WriteLine("Отправка запроса 1...");
+int result2 = server.Fakerequest();
+Console.WriteLine($"Запрос 2 завершен. Ответ: {result2}.");
+
+// Третий запрос  
+Console.WriteLine("Отправка запроса 1...");
+int result3 = server.Fakerequest();
+Console.WriteLine($"Запрос 3 завершен. Ответ: {result3}.");
+
+
+stopwatch.Stop();
+Console.WriteLine($"\\nВсего затрачено времени: {stopwatch.ElapsedMilliseconds} мс.");
+
+
+
+
+Console.WriteLine("=== АСИНХРОННАЯ ВЕРСИЯ ===");
+stopwatch = Stopwatch.StartNew();
+
+Task<int> task1 = server.FakerequestAsync();
+Task<int> task2 = server.FakerequestAsync();
+Task<int> task3 = server.FakerequestAsync();
+
+int[] results = await Task.WhenAll(task1, task2, task3);
+
+Console.WriteLine($"Запрос 1 завершен. Ответ: {results[0]}.");
+Console.WriteLine($"Запрос 2 завершен. Ответ: {results[1]}.");
+Console.WriteLine($"Запрос 3 завершен. Ответ: {results[2]}.");
+
+stopwatch.Stop();
+Console.WriteLine($"\\nВсего затрачено времени: {stopwatch.ElapsedMilliseconds} мс.");
